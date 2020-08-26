597444bb252e3303138c20eb90db4a2e8cb1ba93
MrVenik <vladp1998@gmail.com>
2020-08-27 00:51:56 +0300

    Created custom exceptions

---
+++ Nodum/Core/NodeException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Nodum.Core
+{
+    public class NodeException : Exception
+    {
+        public NodeException()
+        {
+        }
+
+        public NodeException(string message) : base(message)
+        {
+        }
+
+        public NodeException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NodeException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ Nodum/Core/NodePinException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Nodum.Core
+{
+    public class NodePinException : Exception
+    {
+        public NodePinException()
+        {
+        }
+
+        public NodePinException(string message) : base(message)
+        {
+        }
+
+        public NodePinException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NodePinException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
