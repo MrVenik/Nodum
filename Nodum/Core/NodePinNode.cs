b57abadc646de9164b1e46da9f46eda7e43ea1d6
MrVenik <vladp1998@gmail.com>
2020-08-28 20:54:48 +0300

    Create NodePinNode.cs

---
+++ Nodum/Core/NodePinNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nodum.Core
+{
+    [Serializable]
+    public class NodePinNode : Node
+    {
+        public NodePinNode(NodePin nodePin) : base(nodePin.Name)
+        {
+            ProtectedTryAddNodePin(nodePin);
+        }
+    }
+}
